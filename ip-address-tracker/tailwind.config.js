94d89095536fc30b3ddd6f826928b404cf634871
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-03 14:39:01 -0500

    Bases para el proyecto

---
+++ ip-address-tracker/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkP: "#2B2B2B",
+        grayP: "#969696",
+      },
+    },
+  },
+  plugins: [],
+};
