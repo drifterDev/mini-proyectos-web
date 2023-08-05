e58e3ebc67570be04d7d53abd5edfee40545f979
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-08-05 15:32:14 -0500

    Bases para el proyecto

---
+++ space-tourism-website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{html,js}",
+    "./src/scripts/*.{html,js}",
+    "./src/styles/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
